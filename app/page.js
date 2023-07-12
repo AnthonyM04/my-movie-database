1e6628f5185f8379c91dcac0252d2354459171f9
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-07-12 12:20:02 -0400

    hero, fight club

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Hero from './shared/hero'
 
 export default function Home() {
   return (
-    <main className="text-2xl font-semibold text-orange-400">
-      <h1>My Movie Database</h1>
+    <main
+    // className="text-3xl p-8 font-semibold text-orange-400
+    >
+      {/* <h1>My Movie Database</h1> */}
+      <Hero/>
     </main>
   )
 }
