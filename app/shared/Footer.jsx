35aa2ad0c9d82aed45fc02672cefde9ff460305e
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-07-26 11:48:10 -0400

    first commit of today

--- app/shared/Footer.jsx
+++ app/shared/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
-<footer className="footer footer-center p-10 bg-zinc-800 text-orange-500 text-primary-content">
+<footer className="footer footer-center p-10 bg-zinc-80 text-primary-content">
   <div>
     <img width="50" src="footer-logo.svg" className="inline-block fill-current w-40"/>
     <p className="font-semibold">
       Providing reliable film information since 2004
     </p> 
-  {/* </div> 
-  <div>
+  </div> 
+  {/* <div>
     <div className="grid grid-flow-col gap-4">
     </div>
   </div> */}
